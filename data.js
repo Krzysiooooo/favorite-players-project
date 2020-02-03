ded1ef538be902eaee7ffd1af808a8f7904d6dc4
KrzysiOOO <krzysztof.poreba.jr@gmail.com>
2020-02-03 17:05:58 +0100

    Homework + gitignore

--- data.js
+++ data.js
@@ -1,44 +1,58 @@
-//ZADANIE
-// 0.Zacommitowac i wypchnac na Git-huba
-// 1.Dokonczyc funkcje onPlayersSelection
-// 2.Zastosowac wlasciwe wyswietlanie dla klubow
-// 3.Dodac wiecej atrybutow dla gracza
-// 4.Po wejsciu na strone przed wybraniem gracza w prawej sekscji (zawodnika) nie powinno sie wyswietlac nic
 var id = 1;
 const players = [
     {
         id: id++,
-        firstName: "Robert Lewandowski",
+        name: "Robert Lewandowski",
         goals: 350,
         age: 32,
         clubs: ["Znicz Pruszkow", "Lech Poznan", "Borussia Dortmund", "Bayern Monachium"]
     },
     {
         id: id++,
-        firstName: "Jakub Błaszyczkowski",
+        name: "Jakub Błaszyczkowski",
         goals: 61,
         age: 34,
         clubs: ["Wisła Krakow", "Borussia Dortmund", "AC Fiorentina", "VFL Wolfsburg"]
     },
     {
         id: id++,
-        firstName: "Paweł Brożek",
+        name: "Paweł Brożek",
         goals: 152,
         age: 37,
         clubs: ["Wisła Krakow", "Trabzonspor", "Recreativo Huelva"]
     }
 ];
+
 function onPlayerSelection(id) {
+    document.querySelector(".player").hidden = false;
     const player = players.find(item => item.id == id);
     document.querySelector(".player .age").innerText = player.age;
+    document.querySelector(".player .name").innerText = player.name;
+    document.querySelector(".player .goals").innerText = player.goals;
+    document.querySelector(".player .clubs").innerText = player.clubs.join(", ");
 }
+
 const menu = document.querySelector(".list-group");
-players.forEach((item) => {
-    const menuItem = document.createElement("a");
-    menuItem.setAttribute("class", "list-group-item list-group-item-action");
-    menuItem.innerText = item.firstName;
-    menuItem.addEventListener("click", () => {
-        onPlayerSelection(item.id);
+
+function buildMenu() {
+    players.forEach((item) => {
+        const menuItem = document.createElement("a");
+        menuItem.setAttribute("class", "list-group-item list-group-item-action");
+        menuItem.innerText = item.name;
+        menuItem.addEventListener("click", () => {
+            onPlayerSelection(item.id);
+        });
+        menu.appendChild(menuItem);
     });
-    menu.appendChild(menuItem);
-});
+}
+
+function hidePlayer(){
+    document.querySelector(".player").hidden = true;
+}
+
+function init(){
+    hidePlayer();
+    buildMenu();
+}
+
+init();
